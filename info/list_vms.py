6156ec99336209a759f8e3493a3ab1f791eb49a6
skoulouzis <S.Koulouzis@uva.nl>
2021-06-24 14:58:56 +0200

    list vms

--- info/list_vms.py
+++ info/list_vms.py
@@ -1,33 +1,24 @@
+import json
+import sys
+
 import boto3
 
 
 
-
-def list_available_vm_sizes(compute_client, region = 'eu-west-1', minimum_cores = 1, minimum_memory_MB = 768):
-    vm_sizes_list = compute_client.virtual_machine_sizes.list(location=region)
-    for vm_size in vm_sizes_list:
-        # if vm_size.number_of_cores >= int(minimum_cores) and vm_size.memory_in_mb >= int(minimum_memory_MB):
-        print('Name:{0}, Cores:{1}, OSDiskMB:{2}, RSDiskMB:{3}, MemoryMB:{4}, MaxDataDisk:{5}'.format(
-            vm_size.name,
-            vm_size.number_of_cores,
-            vm_size.os_disk_size_in_mb,
-            vm_size.resource_disk_size_in_mb,
-            vm_size.memory_in_mb,
-            vm_size.max_data_disk_count
-        ))
-
-
 if __name__ == '__main__':
+    aws_access_key_id = sys.argv[1]
+    aws_secret_access_key = sys.argv[2]
+    region = sys.argv[3]
     client = boto3.client(
-        'ec2','eu-west-1',
+        'ec2',region,
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
     )
-    response = client.describe_instances()
 
-    print(response)
-    #
-    # azure_client_id = sys.argv[1]
-    # azure_secret = sys.argv[2]
-    # azure_subscription_id = sys.argv[3]
-    # azure_tenant = sys.argv[4]
-    # region = sys.argv[5]
-    # list_available_vm_sizes(client, region = 'EastUS', minimum_cores = 2, minimum_memory_MB = 8192)
+    types = client.describe_instance_types()
+    instance_types = []
+    for type in types['InstanceTypes']:
+        instance_types.append(type)
+
+    print(json.dumps(instance_types))
+
