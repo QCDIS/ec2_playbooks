8de9c19da7709ba80164cf4843768f47dc7256d7
skoulouzis <S.Koulouzis@uva.nl>
2021-06-25 14:32:11 +0200

    list vms

--- info/select_best_flavor.py
+++ info/select_best_flavor.py
@@ -12,15 +12,19 @@ def get_shorter_dist(available_instances, requested_instances):
         min_dist = sys.maxsize
         requested_instance = requested_instances[requested_instance_name]
         requested_vector = np.array([int(requested_instance['mem_size'].split(' ')[0]),
-                                     int(requested_instance['num_cores']),
-                                     int(requested_instance['disk_size'].split(' ')[0])])
+                                     int(requested_instance['num_cores'])])
+                                     # int(requested_instance['disk_size'].split(' ')[0])])
+
         for flavor in flavors:
-            available_vector = np.array([flavor['memory_in_mb'], flavor['number_of_cores'], flavor['resource_disk_size_in_mb']])
-            dist = norm(requested_vector - available_vector)
-            if dist < min_dist:
-                min_dist = dist
-                selected_flavor = {'flavor_name': flavor['name']}
-                selected_flavors[requested_instance_name] = selected_flavor
+            if 'x86_64' in flavor['ProcessorInfo']['SupportedArchitectures']:
+                available_vector = np.array([int(flavor['MemoryInfo']['SizeInMiB']),
+                                             int(flavor['VCpuInfo']['DefaultVCpus'])])
+                                             # int(requested_instance['disk_size'].split(' ')[0])])
+                dist = norm(requested_vector - available_vector)
+                if dist < min_dist:
+                    min_dist = dist
+                    selected_flavor = {'flavor_name': flavor['InstanceType']}
+                    selected_flavors[requested_instance_name] = selected_flavor
 
     return selected_flavors
 
