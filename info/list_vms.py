79a32cacab03814a721bfe2494dd74c094699fa3
skoulouzis <skoulouzis@users.noreply.github.com>
2021-08-31 12:01:23 +0200

    Update list_vms.py

--- info/list_vms.py
+++ info/list_vms.py
@@ -19,6 +19,12 @@ if __name__ == '__main__':
         
     
     t2_nano = {'MemoryInfo':{'SizeInMiB':'0.5'},'VCpuInfo':{'DefaultVCpus':'1'},'name':'t2.nano'}
+    t2_micro = {'MemoryInfo':{'SizeInMiB':'1'},'VCpuInfo':{'DefaultVCpus':'1'},'name':'t2.micro'}
+    t2_small = {'MemoryInfo':{'SizeInMiB':'2'},'VCpuInfo':{'DefaultVCpus':'1'},'name':'t2.small'}
+    t2_medium = {'MemoryInfo':{'SizeInMiB':'4'},'VCpuInfo':{'DefaultVCpus':'2'},'name':'t2.medium'}
+    t2_large = {'MemoryInfo':{'SizeInMiB':'8'},'VCpuInfo':{'DefaultVCpus':'2'},'name':'t2.large'}
+    t2_xlarge = {'MemoryInfo':{'SizeInMiB':'16'},'VCpuInfo':{'DefaultVCpus':'4'},'name':'t2.xlarge'}
+    t2_2xlarge = {'MemoryInfo':{'SizeInMiB':'32'},'VCpuInfo':{'DefaultVCpus':'8'},'name':'t2.2xlarge'}
     instance_types.append(t2_nano)
 
     print(json.dumps(instance_types))
