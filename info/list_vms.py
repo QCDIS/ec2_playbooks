ea196fbd13f9d2f0faa8c84e434b25e20cc742fc
skoulouzis <skoulouzis@users.noreply.github.com>
2021-08-31 12:08:22 +0200

    Update list_vms.py

--- info/list_vms.py
+++ info/list_vms.py
@@ -18,13 +18,13 @@ if __name__ == '__main__':
         instance_types.append(type)
         
     
-    t2_nano = {'MemoryInfo':{'SizeInMiB':'0.5'},'VCpuInfo':{'DefaultVCpus':'1'},'name':'t2.nano'}
-    t2_micro = {'MemoryInfo':{'SizeInMiB':'1'},'VCpuInfo':{'DefaultVCpus':'1'},'name':'t2.micro'}
-    t2_small = {'MemoryInfo':{'SizeInMiB':'2'},'VCpuInfo':{'DefaultVCpus':'1'},'name':'t2.small'}
-    t2_medium = {'MemoryInfo':{'SizeInMiB':'4'},'VCpuInfo':{'DefaultVCpus':'2'},'name':'t2.medium'}
-    t2_large = {'MemoryInfo':{'SizeInMiB':'8'},'VCpuInfo':{'DefaultVCpus':'2'},'name':'t2.large'}
-    t2_xlarge = {'MemoryInfo':{'SizeInMiB':'16'},'VCpuInfo':{'DefaultVCpus':'4'},'name':'t2.xlarge'}
-    t2_2xlarge = {'MemoryInfo':{'SizeInMiB':'32'},'VCpuInfo':{'DefaultVCpus':'8'},'name':'t2.2xlarge'}
+    t2_nano = {'MemoryInfo':{'SizeInMiB':'0.5'},'VCpuInfo':{'DefaultVCpus':'1'},'name':'t2.nano','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
+    t2_micro = {'MemoryInfo':{'SizeInMiB':'1'},'VCpuInfo':{'DefaultVCpus':'1'},'name':'t2.micro','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
+    t2_small = {'MemoryInfo':{'SizeInMiB':'2'},'VCpuInfo':{'DefaultVCpus':'1'},'name':'t2.small','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
+    t2_medium = {'MemoryInfo':{'SizeInMiB':'4'},'VCpuInfo':{'DefaultVCpus':'2'},'name':'t2.medium','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
+    t2_large = {'MemoryInfo':{'SizeInMiB':'8'},'VCpuInfo':{'DefaultVCpus':'2'},'name':'t2.large','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
+    t2_xlarge = {'MemoryInfo':{'SizeInMiB':'16'},'VCpuInfo':{'DefaultVCpus':'4'},'name':'t2.xlarge','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
+    t2_2xlarge = {'MemoryInfo':{'SizeInMiB':'32'},'VCpuInfo':{'DefaultVCpus':'8'},'name':'t2.2xlarge','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
     instance_types.append(t2_nano)
 
     print(json.dumps(instance_types))
