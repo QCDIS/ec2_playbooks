fix wrong units (GB to MB)

info/select_best_flavor.py
@@ -5,42 +5,35 @@ import numpy as np
 from numpy.linalg import norm
 
 
-def get_shorter_dist(available_instances, requested_instances,preferred_family=None):
-    flavors = available_instances
+def get_dist(flavor=None, requested_vector=None, min_dist=None, requested_instance_name=None):
     selected_flavors = {}
+    available_vector = np.array([float(flavor['MemoryInfo']['SizeInMiB']),
+                                 float(flavor['VCpuInfo']['DefaultVCpus'])])
+    # int(requested_instance['disk_size'].split(' ')[0])])
+    dist = norm(requested_vector - available_vector)
+    if dist < min_dist:
+        min_dist = dist
+        if 'InstanceType' in flavor:
+            selected_flavor = {'flavor_name': flavor['InstanceType']}
+        elif 'name' in flavor:
+            selected_flavor = {'flavor_name': flavor['name']}
+        selected_flavors[requested_instance_name] = selected_flavor
+    return selected_flavors
+
+
+def get_shorter_dist(available_instances, requested_instances, preferred_family=None):
+    flavors = available_instances
     for requested_instance_name in requested_instances:
         min_dist = sys.maxsize
         requested_instance = requested_instances[requested_instance_name]
         requested_vector = np.array([float(requested_instance['mem_size'].split(' ')[0]),
                                      float(requested_instance['num_cores'])])
-        # int(requested_instance['disk_size'].split(' ')[0])])
-
         for flavor in flavors:
             if not flavor['BareMetal'] and 'ProcessorInfo' in flavor and 'x86_64' in flavor['ProcessorInfo']['SupportedArchitectures']:
                 if preferred_family and preferred_family in flavor['InstanceType']:
-                    available_vector = np.array([float(flavor['MemoryInfo']['SizeInMiB']),
-                                                 float(flavor['VCpuInfo']['DefaultVCpus'])])
-                        # int(requested_instance['disk_size'].split(' ')[0])])
-                    dist = norm(requested_vector - available_vector)
-                    if dist < min_dist:
-                        min_dist = dist
-                        if 'InstanceType' in flavor:
-                            selected_flavor = {'flavor_name': flavor['InstanceType']}
-                        elif 'name' in flavor:
-                            selected_flavor = {'flavor_name': flavor['name']}
-                        selected_flavors[requested_instance_name] = selected_flavor
+                    selected_flavors = get_dist(flavor, requested_vector, min_dist, requested_instance_name)
                 elif not preferred_family:
-                    available_vector = np.array([float(flavor['MemoryInfo']['SizeInMiB']),
-                                                 float(flavor['VCpuInfo']['DefaultVCpus'])])
-                        # int(requested_instance['disk_size'].split(' ')[0])])
-                    dist = norm(requested_vector - available_vector)
-                    if dist < min_dist:
-                        min_dist = dist
-                        if 'InstanceType' in flavor:
-                            selected_flavor = {'flavor_name': flavor['InstanceType']}
-                        elif 'name' in flavor:
-                            selected_flavor = {'flavor_name': flavor['name']}
-                        selected_flavors[requested_instance_name] = selected_flavor
+                    selected_flavors = get_dist(flavor, requested_vector, min_dist, requested_instance_name)
     return selected_flavors
 
 
@@ -57,6 +50,6 @@ if __name__ == "__main__":
     f = open(requested_instances_file_path, )
     requested_instances = json.load(f)
 
-    selected_flavors = get_shorter_dist(available_instances, requested_instances,preferred_family)
+    selected_flavors = get_shorter_dist(available_instances, requested_instances, preferred_family)
     instances = {'selected_flavors': selected_flavors}
     print(json.dumps(instances))
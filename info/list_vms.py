e6d70ffc0b2b5ae829ae3e8df2b15fbd7809c17f
skoulouzis <S.Koulouzis@uva.nl>
2021-08-31 12:57:44 +0200

    fix wrong units (GB to MB)

--- info/list_vms.py
+++ info/list_vms.py
@@ -12,19 +12,24 @@ if __name__ == '__main__':
         aws_secret_access_key=aws_secret_access_key
     )
 
-    types = client.describe_instance_types(Filters=[{'Name': 'instance-type', 'Values': ["t2.micro", "t3.micro"]}])
+    types = client.describe_instance_types()
     instance_types = []
     for type in types['InstanceTypes']:
         instance_types.append(type)
         
     
-    t2_nano = {'MemoryInfo':{'SizeInMiB':'0.5'},'VCpuInfo':{'DefaultVCpus':'1'},'InstanceType':'t2.nano','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
-    t2_micro = {'MemoryInfo':{'SizeInMiB':'1'},'VCpuInfo':{'DefaultVCpus':'1'},'InstanceType':'t2.micro','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
-    t2_small = {'MemoryInfo':{'SizeInMiB':'2'},'VCpuInfo':{'DefaultVCpus':'1'},'InstanceType':'t2.small','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
-    t2_medium = {'MemoryInfo':{'SizeInMiB':'4'},'VCpuInfo':{'DefaultVCpus':'2'},'InstanceType':'t2.medium','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
-    t2_large = {'MemoryInfo':{'SizeInMiB':'8'},'VCpuInfo':{'DefaultVCpus':'2'},'InstanceType':'t2.large','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
-    t2_xlarge = {'MemoryInfo':{'SizeInMiB':'16'},'VCpuInfo':{'DefaultVCpus':'4'},'InstanceType':'t2.xlarge','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
-    t2_2xlarge = {'MemoryInfo':{'SizeInMiB':'32'},'VCpuInfo':{'DefaultVCpus':'8'},'InstanceType':'t2.2xlarge','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
+    t2_nano = {'MemoryInfo':{'SizeInMiB':'500'},'VCpuInfo':{'DefaultVCpus':'1'},'InstanceType':'t2.nano','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
+    t2_micro = {'MemoryInfo':{'SizeInMiB':'1000'},'VCpuInfo':{'DefaultVCpus':'1'},'InstanceType':'t2.micro','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
+    t2_small = {'MemoryInfo':{'SizeInMiB':'2000'},'VCpuInfo':{'DefaultVCpus':'1'},'InstanceType':'t2.small','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
+    t2_medium = {'MemoryInfo':{'SizeInMiB':'4000'},'VCpuInfo':{'DefaultVCpus':'2'},'InstanceType':'t2.medium','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
+    t2_large = {'MemoryInfo':{'SizeInMiB':'8000'},'VCpuInfo':{'DefaultVCpus':'2'},'InstanceType':'t2.large','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
+    t2_xlarge = {'MemoryInfo':{'SizeInMiB':'16000'},'VCpuInfo':{'DefaultVCpus':'4'},'InstanceType':'t2.xlarge','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
+    t2_2xlarge = {'MemoryInfo':{'SizeInMiB':'32000'},'VCpuInfo':{'DefaultVCpus':'8'},'InstanceType':'t2.2xlarge','BareMetal':False,'ProcessorInfo':{'SupportedArchitectures':'x86_64'}}
     instance_types.append(t2_nano)
+    instance_types.append(t2_micro)
+    instance_types.append(t2_small)
+    instance_types.append(t2_medium)
+    instance_types.append(t2_xlarge)
+    instance_types.append(t2_2xlarge)
 
     print(json.dumps(instance_types))
--- info/select_best_flavor.py
+++ info/select_best_flavor.py
@@ -5,42 +5,35 @@ import numpy as np
 from numpy.linalg import norm
 
 
-def get_shorter_dist(available_instances, requested_instances,preferred_family=None):
-    flavors = available_instances
+def get_dist(flavor=None, requested_vector=None, min_dist=None, requested_instance_name=None):
     selected_flavors = {}
+    available_vector = np.array([float(flavor['MemoryInfo']['SizeInMiB']),
+                                 float(flavor['VCpuInfo']['DefaultVCpus'])])
+    # int(requested_instance['disk_size'].split(' ')[0])])
+    dist = norm(requested_vector - available_vector)
+    if dist < min_dist:
+        min_dist = dist
+        if 'InstanceType' in flavor:
+            selected_flavor = {'flavor_name': flavor['InstanceType']}
+        elif 'name' in flavor:
+            selected_flavor = {'flavor_name': flavor['name']}
+        selected_flavors[requested_instance_name] = selected_flavor
+    return selected_flavors
+
+
+def get_shorter_dist(available_instances, requested_instances, preferred_family=None):
+    flavors = available_instances
     for requested_instance_name in requested_instances:
         min_dist = sys.maxsize
         requested_instance = requested_instances[requested_instance_name]
         requested_vector = np.array([float(requested_instance['mem_size'].split(' ')[0]),
                                      float(requested_instance['num_cores'])])
-        # int(requested_instance['disk_size'].split(' ')[0])])
-
         for flavor in flavors:
             if not flavor['BareMetal'] and 'ProcessorInfo' in flavor and 'x86_64' in flavor['ProcessorInfo']['SupportedArchitectures']:
                 if preferred_family and preferred_family in flavor['InstanceType']:
-                    available_vector = np.array([float(flavor['MemoryInfo']['SizeInMiB']),
-                                                 float(flavor['VCpuInfo']['DefaultVCpus'])])
-                        # int(requested_instance['disk_size'].split(' ')[0])])
-                    dist = norm(requested_vector - available_vector)
-                    if dist < min_dist:
-                        min_dist = dist
-                        if 'InstanceType' in flavor:
-                            selected_flavor = {'flavor_name': flavor['InstanceType']}
-                        elif 'name' in flavor:
-                            selected_flavor = {'flavor_name': flavor['name']}
-                        selected_flavors[requested_instance_name] = selected_flavor
+                    selected_flavors = get_dist(flavor, requested_vector, min_dist, requested_instance_name)
                 elif not preferred_family:
-                    available_vector = np.array([float(flavor['MemoryInfo']['SizeInMiB']),
-                                                 float(flavor['VCpuInfo']['DefaultVCpus'])])
-                        # int(requested_instance['disk_size'].split(' ')[0])])
-                    dist = norm(requested_vector - available_vector)
-                    if dist < min_dist:
-                        min_dist = dist
-                        if 'InstanceType' in flavor:
-                            selected_flavor = {'flavor_name': flavor['InstanceType']}
-                        elif 'name' in flavor:
-                            selected_flavor = {'flavor_name': flavor['name']}
-                        selected_flavors[requested_instance_name] = selected_flavor
+                    selected_flavors = get_dist(flavor, requested_vector, min_dist, requested_instance_name)
     return selected_flavors
 
 
@@ -57,6 +50,6 @@ if __name__ == "__main__":
     f = open(requested_instances_file_path, )
     requested_instances = json.load(f)
 
-    selected_flavors = get_shorter_dist(available_instances, requested_instances,preferred_family)
+    selected_flavors = get_shorter_dist(available_instances, requested_instances, preferred_family)
     instances = {'selected_flavors': selected_flavors}
     print(json.dumps(instances))
