992b7e4a43d7f7152e6e996bca02778648031dec
skoulouzis <koulouzis@gmail.com>
2021-10-14 13:22:10 +0200

    remove dbg msg

--- info/select_best_flavor.py
+++ info/select_best_flavor.py
@@ -4,8 +4,14 @@ import sys
 import numpy as np
 from numpy.linalg import norm
 
+# if not flavor['BareMetal'] and 'ProcessorInfo' in flavor and 'x86_64' in flavor['ProcessorInfo'][
+#     'SupportedArchitectures']:
+#     if preferred_family and preferred_family in flavor['InstanceType']:
+#         selected_flavors.update(get_dist(flavor, requested_vector, min_dist, requested_instance_name))
+#     elif not preferred_family:
 
-def get_shorter_dist(input_available_instances, input_requested_instances):
+
+def get_shorter_dist(input_available_instances=None, input_requested_instances=None, preferred_family=None):
     flavors = input_available_instances
     the_selected_flavors = {}
     for requested_instance_name in input_requested_instances:
@@ -14,13 +20,24 @@ def get_shorter_dist(input_available_instances, input_requested_instances):
         requested_vector = np.array([int(requested_instance['mem_size'].split(' ')[0]),
                                      int(requested_instance['num_cores'])])
         for flavor in flavors:
-            available_vector = np.array(
-                [int(flavor['MemoryInfo']['SizeInMiB']), int(flavor['VCpuInfo']['DefaultVCpus'])])
-            dist = norm(requested_vector - available_vector)
-            if dist < min_dist:
-                min_dist = dist
-                selected_flavor = {'flavor_name': flavor['InstanceType']}
-                the_selected_flavors[requested_instance_name] = selected_flavor
+            if not flavor['BareMetal'] and 'ProcessorInfo' in flavor and 'x86_64' in flavor['ProcessorInfo'][
+                'SupportedArchitectures']:
+                if preferred_family and preferred_family in flavor['InstanceType']:
+                    available_vector = np.array(
+                        [int(flavor['MemoryInfo']['SizeInMiB']), int(flavor['VCpuInfo']['DefaultVCpus'])])
+                    dist = norm(requested_vector - available_vector)
+                    if dist < min_dist:
+                        min_dist = dist
+                        selected_flavor = {'flavor_name': flavor['InstanceType']}
+                        the_selected_flavors[requested_instance_name] = selected_flavor
+                elif not preferred_family:
+                    available_vector = np.array(
+                        [int(flavor['MemoryInfo']['SizeInMiB']), int(flavor['VCpuInfo']['DefaultVCpus'])])
+                    dist = norm(requested_vector - available_vector)
+                    if dist < min_dist:
+                        min_dist = dist
+                        selected_flavor = {'flavor_name': flavor['InstanceType']}
+                        the_selected_flavors[requested_instance_name] = selected_flavor
 
     return the_selected_flavors
 
@@ -28,6 +45,9 @@ def get_shorter_dist(input_available_instances, input_requested_instances):
 if __name__ == "__main__":
     available_instances_file_path = sys.argv[1]
     requested_instances_file_path = sys.argv[2]
+    preferred_family = None
+    if len(sys.argv) == 4:
+        preferred_family = sys.argv[3]
 
     f = open(available_instances_file_path, )
     available_instances = json.load(f)
@@ -35,6 +55,7 @@ if __name__ == "__main__":
     f = open(requested_instances_file_path, )
     requested_instances = json.load(f)
 
-    selected_flavors = get_shorter_dist(available_instances, requested_instances)
+    selected_flavors = get_shorter_dist(input_available_instances=available_instances,
+                                        input_requested_instances=requested_instances,preferred_family=preferred_family)
     instances = {'selected_flavors': selected_flavors}
     print(json.dumps(instances))
